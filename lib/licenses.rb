59250f5918db94429da0eb47e78cc5425bd67851
Ryan Scott Lewis <ryan@rynet.us>
2016-07-18 05:51:43 -0400

    Licenses#find_license documentation

--- lib/licenses.rb
+++ lib/licenses.rb
@@ -9,6 +9,14 @@ module Licenses
   class << self
     extend Forwardable
 
+    # @method find_license
+    # Find a license by it's attributes
+    #
+    # @example
+    #   Licenses.find_license(shortname: :mit) # => #<License:0x000...>
+    # @return [License, nil]
+    # @api public
+    # @since 0.1.0
     def_delegator License, :find, :find_license
 
     # Get the path to the root directory for the {Licenses} project
