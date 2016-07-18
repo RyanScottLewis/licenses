f4d0e853f3756d9095ad04d9350ef7c23ef737c7
Ryan Scott Lewis <ryan@rynet.us>
2016-07-18 12:40:59 -0400

    Helpers::Formatting#format_underscore: Replace periods with underscores

--- lib/licenses/helpers/formatting.rb
+++ lib/licenses/helpers/formatting.rb
@@ -28,6 +28,7 @@ module Licenses
              .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
              .gsub(/([a-z\d])([A-Z])/, '\1_\2')
              .tr('-', '_')
+             .tr('.', '_')
              .gsub(/\s/, '_')
              .gsub(/__+/, '_')
              .downcase
