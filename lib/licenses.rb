edb2a30d7755e80b0f02276b8fdf7c3074f4c052
Ryan Scott Lewis <ryan@rynet.us>
2016-07-18 05:50:21 -0400

    Licenses#path

--- lib/licenses.rb
+++ lib/licenses.rb
@@ -1,3 +1,24 @@
+require 'pathname'
+require 'forwardable'
+
 require 'licenses/version'
 require 'licenses/license'
 require 'licenses/organization'
+
+# Provides an easy way to generate common project license files.
+module Licenses
+  class << self
+    extend Forwardable
+
+    def_delegator License, :find, :find_license
+
+    # Get the path to the root directory for the {Licenses} project
+    #
+    # @return [Pathname]
+    # @api private
+    # @since 0.1.0
+    def path
+      @path ||= Pathname.new(__FILE__).join(*%w(.. ..)).expand_path
+    end
+  end
+end
