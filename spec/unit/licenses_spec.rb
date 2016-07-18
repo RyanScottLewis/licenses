3f7b83a8c1b78f0715eb7bf20db6645ef22ae2a2
Ryan Scott Lewis <ryan@rynet.us>
2016-07-18 12:41:27 -0400

    Licenses: Slightly better specs

--- spec/unit/licenses_spec.rb
+++ spec/unit/licenses_spec.rb
@@ -2,12 +2,20 @@ describe Licenses do
   describe 'VERSION' do
     subject { Licenses::VERSION }
 
-    it { should_not be_nil }
+    example 'it should be set' do
+      expect(subject).not_to be_nil
+    end
+
+    example 'it should be frozen' do
+      expect(subject).to be_frozen
+    end
   end
 
   describe '#path' do
-    subject { Licenses.path.to_s }
+    subject { Licenses.path }
 
-    it { should eq(File.expand_path(File.join(%w(.. .. ..)), __FILE__)) }
+    example 'it should return correctly' do
+      expect(subject.to_s).to eq(File.expand_path(File.join(%w(.. .. ..)), __FILE__))
+    end
   end
 end
