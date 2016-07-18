e8671ee2f44219e25d2a0534baec1e5c6a4921ea
Ryan Scott Lewis <ryan@rynet.us>
2016-07-18 06:34:45 -0400

    License spec

---
+++ spec/unit/licenses/license_spec.rb
@@ -0,0 +1,132 @@
+require 'support/license_helpers'
+
+describe Licenses::License do
+  include LicenseHelpers
+
+  describe '#initialize' do
+    context 'when a name is given' do
+      example 'then it should not raise an error' do
+        expect { build_license }.not_to raise_error
+      end
+    end
+
+    context 'when a name is not given' do
+      example 'then it hould raise an error' do
+        expect { build_license(name: nil) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#name' do
+    subject { build_license.name }
+
+    example 'it should return correctly' do
+      should eq('Test License')
+    end
+  end
+
+  describe '#name=' do
+    subject { build_license }
+
+    example 'it should set correctly' do
+      subject.name = 'My License'
+
+      expect(subject.name).to eq('My License')
+    end
+
+    context 'when the value is surrounded spaces' do
+      example 'then it should be stripped' do
+        subject.name = '   My License  '
+
+        expect(subject.name).to eq('My License')
+      end
+
+      context 'and it is empty after stripping' do
+        example 'then it should be raise an error' do
+          expect { subject.name = "   \r  \n  \x0 \r\n " }.to raise_error(ArgumentError)
+        end
+      end
+    end
+  end
+
+  describe '#shortname' do
+    context 'when a shortname is set' do
+      subject { build_license(shortname: :tl).shortname }
+
+      example 'then it should return correctly' do
+        should eq(:tl)
+      end
+    end
+
+    context 'when a shortname is unset' do
+      subject { build_license.shortname }
+
+      example 'then it should be generated from the name' do
+        should eq(:test_license)
+      end
+    end
+  end
+
+  describe '#shortname=' do
+    subject { build_license }
+
+    example 'it should set correctly' do
+      subject.shortname = 'Awesome License'
+      expect(subject.shortname).to eq(:awesome_license)
+
+      subject.shortname = :" Foobar my_license WowWhee"
+      expect(subject.shortname).to eq(:foobar_my_license_wow_whee)
+    end
+  end
+
+  describe '#url' do
+    subject { build_license.url }
+
+    example 'it should return correctly' do
+      should eq(nil)
+    end
+  end
+
+  describe '#url=' do
+    subject { build_license }
+
+    example 'then it should set correctly' do
+      subject.url = 'https://example.org/license.txt'
+      expectation = URI.parse('https://example.org/license.txt')
+
+      expect(subject.url).to eq(expectation)
+    end
+  end
+
+  describe '#template' do
+    context 'when a template is set' do
+      subject { build_license(template: :tl).template }
+
+      example 'then it should return correctly' do
+        should eq('tl')
+      end
+    end
+
+    context 'when a template is unset' do
+      subject { build_license }
+
+      example 'then it should be generated from the shortname' do
+        expect(subject.template).to eq('test_license')
+        subject.shortname = :awesome_license
+        expect(subject.template).to eq('awesome_license')
+      end
+    end
+  end
+
+  describe '#template=' do
+    subject { build_license }
+
+    example 'it should set correctly' do
+      subject.template = 'Awesome License'
+      expect(subject.template).to eq('awesome_license')
+
+      subject.template = :" Foobar my_license WowWhee"
+      expect(subject.template).to eq('foobar_my_license_wow_whee')
+    end
+  end
+end
